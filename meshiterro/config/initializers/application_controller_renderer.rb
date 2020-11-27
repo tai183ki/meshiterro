2fb172f3db309720db7b8b92de41cc1a61a6763e
tai183ki <tai183ki.9656@gmail.com>
2020-11-27 23:30:57 +0000

    [4-4]Start

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dd7ec7ceb71e9542946970fcbeaf1ebee85003258e2c80bebd37edc58542f67cb0bef4475361be641cff89b924b0bf5bbf3980477a4874b099a234a4522e3a1b'
